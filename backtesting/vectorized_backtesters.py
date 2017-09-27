0a277bf7e77b69f6dcd5cd55a55753bad3e87556
Charlio Xu <chalioxumykj@gmail.com>
2017-09-27 10:52:46 -0700

    feat: modify SMAVectorizedBacktester for easier grid parameter search

--- backtesting/vectorized_backtesters.py
+++ backtesting/vectorized_backtesters.py
@@ -5,28 +5,31 @@ from pandas_datareader import data as web
 
 # Simple Moving Average
 class SMAVectorizedBacktester(object):
-    def __init__(self, symbol, start, end, SMA1, SMA2, tc):
+    def __init__(self, symbol, start, end, tc):
         self.symbol = symbol
         self.start = start
         self.end = end
-        self.SMA1 = SMA1
-        self.SMA2 = SMA2
         self.tc = tc
         self.get_data()
     
     def get_data(self):
         raw = pd.DataFrame(web.DataReader(self.symbol, start=self.start,
                                           end=self.end, data_source='google')['Close'])
-        raw['SMA1'] = raw['Close'].rolling(self.SMA1).mean()
-        raw['SMA2'] = raw['Close'].rolling(self.SMA2).mean()
         raw['Returns'] = np.log(raw['Close'] / raw['Close'].shift(1))
         self.data = raw.dropna()
         
-    def plot_data(self):
-        self.data['Close'].plot(figsize(10, 6), title=self.symbol)
+    def plot_data(self, cols='Close'):
+        self.data[cols].plot(figsize(10, 6), title=self.symbol)
         
-    def run_strategy(self):
-        data = self.data.copy()
+    def prepare_data(self):
+        self.data['SMA1'] = self.data['Close'].rolling(self.SMA1).mean()
+        self.data['SMA2'] = self.data['Close'].rolling(self.SMA2).mean()
+        
+    def run_strategy(self, SMA1, SMA2):
+        self.SMA1 = SMA1
+        self.SMA2 = SMA2
+        self.prepare_data()
+        data = self.data.dropna().copy()
         data['Position'] = np.where(data['SMA1'] > data['SMA2'], 1, -1)
         data['Strategy'] = data['Position'].shift(1) * data['Returns']
         data.dropna(inplace=True)
@@ -41,12 +44,26 @@ class SMAVectorizedBacktester(object):
         self.results[['CReturns', 'CStrategy', 'Position']].plot(secondary_y='Position', figsize=(10, 6))
         
 '''        
-sma = SMAVectorizedBacktester(symbol='AAPL', start='2012-1-1', end='2017-8-31,
-                              SMA1=42, SMA2=252, tc=0.01) # alternative: SMA1=30, SMA2=150
-sma.run_strategy()
+sma = SMAVectorizedBacktester(symbol='AAPL', start='2012-1-1', end='2017-8-31, tc=0.01) 
+sma.run_strategy(SMA1=42, SMA2=252) # alternative: SMA1=30, SMA2=150
 sma.plot_results()
 '''        
-        
+
+# Brute Force Optimization
+import itertools as it
+
+results = pd.DataFrame()
+for SMA1, SMA2 in it.product(range(10, 31, 10), range(50, 101, 10)):
+    print(SMA1, SMA2)
+    result = sma.run_strategy(SMA1, SMA2)
+    print(result)
+    results = results.append(pd.DataFrame({'SMA1': SMA1, 'SMA2': SMA2,
+                                           'CReturns': result['CReturns'],
+                                           'CStrategy': result['CStrategy']},
+                                           index=[0]),
+                             ignore_index=True)
+# results['CStrategy'] > results['CReturns']
+    
         
         
     
