8568c9f89eaa8569fa5f1a6efbd8c9dc94c22ca7
Charlio Xu <chalioxumykj@gmail.com>
2017-09-27 10:19:12 -0700

    refactor: implement class SMAVectorizedBacktestor

--- backtesting/vectorized_backtesters.py
+++ backtesting/vectorized_backtesters.py
@@ -2,56 +2,55 @@ import numpy as np
 import pandas as pd
 from pandas_datareader import data as web
 
-symbol = 'AAPL'
-data = pd.DataFrame(web.DataReader(symbol, data_source='google'['Close']))
-# data.info()
 
-# Strategy Formulation
-
-#Simple Moving Average
-data['SMA1'] = data['Close'].rolling(42).mean() # why 42 days?
-data['SMA2'] = data['Close'].rolling(252).mean() # one trading year
-# data.head()
-# data.tail()
-
-''' 
-Plot:
-from pylab import plt
-plt.style.use('ggplot')
-%matplotlib inline
-data.plot(figsize=(10, 6));
-'''
-
-data['Position'] = np.where(data['SMA1'] > data['SMA2'], 1, -1)
-
-# data.dropna().plot(figsize=(10, 6), secondary_y='Position')
-
-
-# Strategy Backtesting
-data['Returns'] = np.log(data['Close'] / data['Close'].shift(1))
-data.dropna(inplace=True)
-data['Strategy'] = data['Position'].shift(1) * data['Returns']
-data.dropna(inplace=True)
-data['CReturns'] = data['returns'].cumsum()
-data['CStrategy'] = data['Strategy'].cumsum()
-
-#final_returns = np.exp(data[['CReturns', 'CStrategy']].ix[-1]) - 1
-# data[['CReturns', 'CStrategy', 'Position']].apply(np.exp).plot(secondary_y='Position', figsize=(10, 6));
-
-
-# Transaction Costs
-trades = (data['Position'].diff().fillna(0) != 0)
-data[trades] # days to trade
-tc = 0.01
-data['Strategy'] = np.where(trades, data['Strategy'] - tc, data['Strategy'])
-data['CStrategy'] = data['Strategy'].cumsum()
-final_returns_tc = np.exp(data[['CReturns', 'CStrategy']].ix[-1]) - 1
-# data[['CReturns', 'CStrategy', 'Position']].apply(np.exp).plot(secondary_y='Position', figsize=(10, 6));
-
-
-# Object-oriented design
-
-
+# Simple Moving Average
+class SMAVectorizedBacktester(object):
+    def __init__(self, symbol, start, end, SMA1, SMA2, tc):
+        self.symbol = symbol
+        self.start = start
+        self.end = end
+        self.SMA1 = SMA1
+        self.SMA2 = SMA2
+        self.tc = tc
+        self.get_data()
+    
+    def get_data(self):
+        raw = pd.DataFrame(web.DataReader(self.symbol, start=self.start,
+                                          end=self.end, data_source='google')['Close'])
+        raw['SMA1'] = raw['Close'].rolling(self.SMA1).mean()
+        raw['SMA2'] = raw['Close'].rolling(self.SMA2).mean()
+        raw['Returns'] = np.log(raw['Close'] / raw['Close'].shift(1))
+        self.data = raw.dropna()
+        
+    def plot_data(self):
+        self.data['Close'].plot(figsize(10, 6), title=self.symbol)
+        
+    def run_strategy(self):
+        data = self.data.copy()
+        data['Position'] = np.where(data['SMA1'] > data['SMA2'], 1, -1)
+        data['Strategy'] = data['Position'].shift(1) * data['Returns']
+        data.dropna(inplace=True)
+        trades = (data['Position'].diff().fillna(0) != 0)
+        data['Strategy'] = np.where(trades, data['Strategy'] - self.tc, data['Strategy'])
+        data['CReturns'] = data['Returns'].cumsum().apply(np.exp)
+        data['CStrategy'] = data['Strategy'].cumsum().apply(np.exp)
+        self.results = data
+        return data[['CReturns', 'CStrategy']].iloc[-1] - 1
+        
+    def plot_results(self):
+        self.results[['CReturns', 'CStrategy', 'Position']].plot(secondary_y='Position', figsize=(10, 6))
+        
+'''        
+sma = SMAVectorizedBacktester(symbol='AAPL', start='2012-1-1', end='2017-8-31,
+                              SMA1=42, SMA2=252, tc=0.01) # alternative: SMA1=30, SMA2=150
+sma.run_strategy()
+sma.plot_results()
+'''        
+        
+        
+        
+    
+    
 
 
 
